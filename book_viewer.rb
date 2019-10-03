cd471c0a73b6432d58e70723c733370f76545b21
mandlerm <amaya.melissa@gmail.com>
2019-10-02 21:51:28 -0500

    redirect working

--- book_viewer.rb
+++ book_viewer.rb
@@ -2,21 +2,37 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+before do
+  @contents = File.readlines('data/toc.txt', chomp: true)
+end
+
+helpers do
+  def in_paragraphs(chapter)
+    chapter.split("\n\n").map { |line| "<p>#{line}</p>" }.join
+  end
+end
+
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
-  @contents = File.readlines('data/toc.txt', chomp: true)
   erb :home
 end
 
-get "/chapters/:num" do |num|
-  @contents = File.readlines('data/toc.txt', chomp: true)
-  @chapter = File.read("data/chp#{num}.txt")
-  chapter_name = @contents[num.to_i - 1]
-  @title = "Chapter #{num}: #{chapter_name}"
+get "/chapters/:number" do
+  number = params[:number].to_i
+  chapter_name = @contents[number - 1]
+
+  redirect "/" unless (1..@contents.size).cover? number
+
+  @title = "Chapter #{number}: #{chapter_name}"
+  @chapter = File.read("data/chp#{number}.txt")
 
   erb :chapter
 end
 
 get "/show/:name" do
   params[:name]
+end
+
+not_found do
+  redirect "/"
 end
