6f14f55eec83fdb865695d090679c09bdf189231
mandlerm <amaya.melissa@gmail.com>
2019-10-02 21:03:56 -0500

    route to chapters

--- book_viewer.rb
+++ book_viewer.rb
@@ -4,14 +4,19 @@ require "tilt/erubis"
 
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
-  @toc = File.readlines('data/toc.txt', chomp: true)
-  erb :chapter
+  @contents = File.readlines('data/toc.txt', chomp: true)
+  erb :home
 end
 
 get "/chapters/:num" do |num|
-  @title = "Chapter #{num}"
-  @toc = File.readlines('data/toc.txt', chomp: true)
-  @chapter_contents = File.read("data/chp#{num}.txt")
+  @contents = File.readlines('data/toc.txt', chomp: true)
+  @chapter = File.read("data/chp#{num}.txt")
+  chapter_name = @contents[num.to_i - 1]
+  @title = "Chapter #{num}: #{chapter_name}"
 
   erb :chapter
+end
+
+get "/show/:name" do
+  params[:name]
 end
